1a2c3fca443c3741820ae7641bec2c12b34aef46
Caroline <carolinesitter24@gmail.com>
2022-03-17 19:29:35 -0500

    Thirteenth Function Fixed

--- src/list-slicing/list_operations.py
+++ src/list-slicing/list_operations.py
@@ -195,8 +195,8 @@ def delete_third_and_seventh(input_list):
     ['Do', 'Re', 'Fa', 'So', 'La', 'Do']
 
     """
-    del input_list[2]
-    del input_list[6]
+    input_list[2:3] = []
+    input_list[5:6] = []
 
 
 def delete_middle(input_list):
